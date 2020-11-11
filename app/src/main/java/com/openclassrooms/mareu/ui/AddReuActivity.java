b7553cec4d44f1264118e6d6ef8b43e7eb8749fd
Pauline <p.cellier91@gmail.com>
2020-11-11 20:36:34 +0100

    Commit style calendar

--- app/src/main/java/com/openclassrooms/mareu/ui/AddReuActivity.java
+++ app/src/main/java/com/openclassrooms/mareu/ui/AddReuActivity.java
@@ -1,13 +1,17 @@
 package com.openclassrooms.mareu.ui;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -22,8 +26,10 @@ import com.openclassrooms.mareu.di.DI;
 import com.openclassrooms.mareu.model.Reunion;
 import com.openclassrooms.mareu.service.ReunionApiService;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 
 import butterknife.BindView;
@@ -38,8 +44,6 @@ public class AddReuActivity extends AppCompatActivity {
     TextInputLayout nameInput;
     @BindView(R.id.lieuLyt)
     TextInputLayout lieuInput;
-    @BindView(R.id.datePicker1)
-    DatePicker heureInput;
     @BindView(R.id.aboutItLyt)
     TextInputLayout aboutItInput;
     @BindView(R.id.emailLyt)
@@ -50,6 +54,8 @@ public class AddReuActivity extends AppCompatActivity {
     private ReunionApiService mApiService;
     private String mNeighbourImage;
     private DatePicker picker;
+    private Calendar myCalendar;
+    private TextView dateview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +65,8 @@ public class AddReuActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mApiService = DI.getReunionApiService();
-        picker=(DatePicker)findViewById(R.id.datePicker1);
         init();
+        getDate();
     }
 
     @Override
@@ -92,10 +98,34 @@ public class AddReuActivity extends AppCompatActivity {
     }
 
     private void getDate(){
-        final Calendar cldr = Calendar.getInstance();
-        int day = cldr.get(Calendar.DAY_OF_MONTH);
-        int month = cldr.get(Calendar.MONTH);
-        int year = cldr.get(Calendar.YEAR);
+        myCalendar = Calendar.getInstance();
+
+        dateview = (TextView) findViewById(R.id.datePickerLyt);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        dateview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddReuActivity.this, R.style.DialogTheme, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel(){
+        String myFormat = "dd/MM/YY";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.FRANCE);
+
+        dateview.setText(sdf.format(myCalendar.getTime()));
     }
 
     @OnClick(R.id.create)
